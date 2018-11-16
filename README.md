34481b8c07773c25fe637bf5a5cc718b731e8168
traviegotswag <travisng@hotmail.sg>
2018-11-16 17:39:40 +0800

    installed devise

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+articles.error.any?
+articles.error.full_messages
+
+
+
+
 # README
 
 This README would normally document whatever steps are necessary to get the
---
+++ app/models/profile.rb
@@ -0,0 +1,4 @@
+# class Profile < ActiveRecord: :Base 
+#     validates :username, presence: true
+#     validates :password, presence: true, length: { minimum: 5, maximum: 20 }
+# end 
